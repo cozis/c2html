49cb17104d68302b72e37f07c6b5efac553eeba7
cozis <heibyob@gmail.com>
2022-05-08 02:58:58 +0200

    more docs

--- c2html.c
+++ c2html.c
@@ -24,11 +24,12 @@
  * | THE  AUTHORS  BE LIABLE FOR ANY CLAIM, DAMAGES  OR  OTHER  | *
  * | LIABILITY, WHETHER  IN  AN  ACTION  OF  CONTRACT, TORT  OR | *
  * | OTHERWISE, ARISING  FROM, OUT  OF  OR  IN  CONNECTION WITH | *
- * | THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. | * *
+ * | THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. | *  *
  * +------------------------------------------------------------+--+ *
  * | For more information, please refer to <http://unlicense.org/> | *
  * +---------------------------------------------------------------+ *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
--- c2html.h
+++ c2html.h
@@ -1,3 +1,23 @@
-
+/* Takes as input a string of C code [str] of length
+ * [len] and returns the same C code but annotated
+ * with HTML tags. The returned string must be freed
+ * using [free]. The class names of each tag are
+ * prefixed with the value provided through [prefix].
+ * If [prefix] is NULL, then no prefix is used.
+ *
+ * If the function fails, NULL is returned and a
+ * human-readable description of the error is
+ * returned through the output parameter [error].
+ * The returned error string doesn't need to be
+ * deallocated. If you're not interested in the
+ * error description, you can just provide NULL.
+ *
+ * The [str] string isn't assumed to be zero-terminated
+ * and if it's NULL, an empty string is assumed.
+ *
+ * If [len] is negative, then [str] is assumed 
+ * to be zero-terminated and it's length is calculated
+ * using [strlen].
+ */
 char *c2html(const char *str, long len, 
              const char *prefix, const char **error);
